d4a07df3d62c96e7bfb7efa17d90a7968839a697
K0966245 <tanawan.ko@kbtg.tech>
2023-11-02 15:33:44 +0700

    init

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/tanawanko/go-nk
+
+go 1.17
+
+
+// replace go-common-lib => github.com/kbtg-ksoft-698-01/go-common-lib v0.3.0
---
+++ hello/hello.go
@@ -0,0 +1,8 @@
+package hello
+
+import "fmt"
+
+// HelloWorld prints "Hello, World!" to the console.
+func HelloWorld() {
+    fmt.Println("Hello, World!")
+}
